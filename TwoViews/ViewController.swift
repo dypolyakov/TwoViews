dc4e301ce117838f92d1150da25bef56bff5d8d2
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2022-10-28 14:29:00 +0500

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Дмитрий Поляков on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
